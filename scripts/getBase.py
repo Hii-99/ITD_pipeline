caf080cd8f87d7a8e4d98228f333d384c9a190b9
Hii-99 <hiiluann994q@gmail.com>
2024-04-12 20:54:57 +0800

    getBase: test done!

--- scripts/getBase.py
+++ scripts/getBase.py
@@ -2,7 +2,7 @@ from pyfaidx import Fasta
 
 def get_genome_base(chromosome, position):
     # Load the human genome sequence
-    genome = Fasta("path_to_your_human_genome.fa")  # Provide the path to your human genome fasta file
+    genome = Fasta("/home/hiiluann99/bin/GenomonITDetector38/GRCh38.d1.vd1.fa")  # Provide the path to your human genome fasta file
 
     # Access the sequence at the specified chromosome and position
     sequence = genome[chromosome][position - 1]  # Adjust position to 0-based index
@@ -10,7 +10,9 @@ def get_genome_base(chromosome, position):
     return sequence
 
 # Example usage
-chromosome = "chrX"
-position = 12345
-base = get_genome_base(chromosome, position)
-print("Base at position", position, ":", base)
+chr = ["chr1", "chr1", "chr1"]
+pos = [2290573, 2303242, 3200983]
+
+for chro, posi in zip(chr,pos):
+    base = get_genome_base(chro, posi)
+    print("Base at position", posi, ":", base)
